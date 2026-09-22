a0284ed973057ccf6bea0357bc626c84696f908e
Gorox_9148 <kaliekperovartem@gmail.com>
2026-09-22 19:38:37 +0300

    Fixed buffer

--- code/main.cpp
+++ code/main.cpp
@@ -8,33 +8,38 @@
 #include "sorting.cpp"
 #include "comparators.cpp"
 #include "wrappers.cpp"
+#include <cstdlib>
 
 
 
 int main(int argc, char *argv[]) {
 
-    if (argc != 2) {
+    if (argc != 3) {
         fprintf(stderr, "Error: wrong arguments");
+        return -1;
     }
 
     size_t strings_count = 0;
     char *filename = argv[1];
+    char *filename_out = argv[2];
 
     struct stat text_stat = {};
     safe_stat(filename, &text_stat); //TODO: function - done
     __off_t file_size = text_stat.st_size;
     __blksize_t optimal_block_size = text_stat.st_blksize;
 
-    char *buffer = (char*) safe_calloc((size_t) file_size + 1, sizeof(char)); // +1 for \0 at the end
+    char *buffer = (char*) safe_calloc((size_t) file_size + 2, sizeof(char)); // +2 for \0 at the start and the end
+    buffer[0] = '\0';
+    ++buffer;
 
     FILE *fp = safe_fopen(filename, "rb"); //TODO: check null - done
     strings_count = read_lines_from_file_to_buffer(fp, buffer, optimal_block_size); //TODO: temp buf?? - done
     fclose(fp);
 
     char **strings_ptrs_array = (char**) safe_calloc(strings_count, sizeof(char*));
-    assign_ptrs_from_buffer_to_strings_array(strings_ptrs_array, buffer, (size_t) file_size + 1);
+    assign_ptrs_from_buffer_to_strings_array(strings_ptrs_array, buffer, (size_t) file_size + 2);
 
-    FILE *fp_out = safe_fopen("output.txt", "wb");
+    FILE *fp_out = safe_fopen(filename_out, "wb");
 
     merge_sort_strings(strings_ptrs_array, strings_count, MAX_STR_LEN, comparator_strings_increase);
     print_string_array_with_message_to_file(fp_out, strings_ptrs_array, strings_count, NULL);
@@ -48,7 +53,7 @@ int main(int argc, char *argv[]) {
 
     fclose(fp_out);
 
-    free(buffer);
+    free(buffer - 1);
     free(strings_ptrs_array);
 
     return 0;
